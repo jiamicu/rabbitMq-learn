a20db30dda00cb4fc966d97bd554f515a533b967
xiawei <xiawei@idwzx.com>
2016-11-06 19:57:43 +0800

    sent rabbitmq a message

---
+++ send.py
@@ -0,0 +1,11 @@
+#此为rabbitmq练习工程
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+channel = connection.channel()
+channel.queue_declare(queue='hello')
+channel.basic_publish(exchange='',
+                      routing_key='hello',
+                      body='Hello World!')
+print(" [x] Sent 'Hello World!'")
+connection.close()
